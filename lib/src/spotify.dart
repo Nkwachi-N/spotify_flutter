8f9f4ce70a6fc17e828b6d096148483a71f5b9d7
Nkwachi <nkwachinwamaghinna@gmail.com>
2022-09-12 05:28:28 +0100

    Create spotify.dart

---
+++ lib/src/spotify.dart
@@ -0,0 +1,19 @@
+import 'package:spotify_flutter/src/auth_service.dart';
+
+class Spotify {
+  final _authService = AuthService();
+
+  Future<bool> authenticate({
+    required String redirectUri,
+    required String clientId,
+    String state = 'HappyBaby247',
+    required String callbackUrlScheme,
+    required String secretKey,
+  }) =>
+      _authService.authorize(
+        redirectUri: redirectUri,
+        clientId: clientId,
+        callbackUrlScheme: callbackUrlScheme,
+        secretKey: secretKey,
+      );
+}
