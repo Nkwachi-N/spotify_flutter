882cefd0ece1fef33d764b2eac8ed0b6ba7276e7
Nkwachi <nkwachinwamaghinna@gmail.com>
2022-09-19 18:10:04 +0100

    include artist export

--- lib/src/core/models/models.dart
+++ lib/src/core/models/models.dart
@@ -3,3 +3,4 @@ export 'image/image.dart';
 export 'explicit_content/explicit_content.dart';
 export 'external_urls/external_urls.dart';
 export 'followers/followers.dart';
+export 'artist/artist.dart';
